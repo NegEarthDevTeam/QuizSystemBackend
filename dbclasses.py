8e260950463c6e77b4a2c7d6106a7fd813523304
Sam Malcolm <samalcolm101@gmail.com>
2021-05-12 16:30:13 +0100

    user creation

--- dbclasses.py
+++ dbclasses.py
@@ -1,10 +1,36 @@
+import datetime
 import main
 
 db = main.db
 
 class HostUser(main.db.Document):
-    name = db.StringField()
+    firstName = db.StringField()
+    lastName = db.StringField()
     email = db.StringField()
     passwordHash = db.StringField()
+    created = datetime.datetime.now()
+    lastEdit = datetime.datetime.now()
 
-    
+
+    def to_json(self):
+        return {
+            '_id' : str(self.pk),
+            'First Name' : self.firstName,
+            'Last Name' : self.lastName,
+            'Email' : self.email,
+            'Creation Date' : self.created,
+            'Edit Date' : self.lastEdit
+        }
+
+class TestUser(main.db.Document):
+    email = db.StringField()
+    created = datetime.datetime.now()
+    lastEdit = datetime.datetime.now()
+
+    def to_json(self):
+        return {
+            '_id': str(self.pk),
+            'Email': self.email,
+            'Creation Date': self.created,
+            'Edit Date': self.lastEdit
+        }
