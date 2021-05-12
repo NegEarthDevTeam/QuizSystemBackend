ce5d25852dd2ae24a45eb639054d8c131d8741f0
Sam Malcolm <samalcolm101@gmail.com>
2021-05-12 14:31:14 +0100

    push to you

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from flask_socketio import *
 from flask_mongoengine import *
 import string
 import random
+import logic
+import globals
 
 app = Flask(__name__)
+
+globals.init()
 
 app.config["MONGODB_SETTINGS"] = {
     "db": "quizsystemdb",
@@ -12,13 +16,12 @@ app.config["MONGODB_SETTINGS"] = {
     "port": 27017
 }
 
+global db
 db = MongoEngine(app)
 
 app.secret_key = "quizSystemSecretKey"
 
 socketio = SocketIO(app, cors_allowed_origins='*')
-
-socketio = SocketIO(app)
 
 # create room is actually just a string generator that then initiates the quiz in the DB
 # check that the room code isn't currently in sure with any of the other active quizzes
@@ -26,9 +29,14 @@ socketio = SocketIO(app)
 
 @socketio.event
 def createRoom(data):
-    quizId = ''.join(random.choice(string.ascii_lowercase) for i in range(6))
-    print(f'room created with quiz ID: {quizId}')
-    send(f'ID {quizId}')
+    while True:
+        quizId = ''.join(random.choice(string.ascii_lowercase)
+                         for i in range(6))
+        if not logic.checkRoomExists(quizId):
+            logic.registerRoomExists(quizId)
+            print(f'room created with quiz ID: {quizId}')
+            send(f'ID {quizId}')
+            break
 
 
 # join room
@@ -37,7 +45,8 @@ def on_join(data):
     username = data['username']
     room = data['room']
     join_room(room)
-    send(username + ' has entered the quizspace.', to=room, )
+    print('the join event was run')
+    send(f'{username} has entered the quizspace', to=room, )
 
 # exit room
 
@@ -47,6 +56,7 @@ def on_leave(data):
     username = data['username']
     room = data['room']
     leave_room(room)
+    print('the ')
     send(username + ' has left the quizspace.', to=room)
 
 # submit quiz answer
@@ -72,6 +82,30 @@ def sendQuestion(data):
 def finishQuiz(data):
     room = data['room']
     close_room(room)
+
+
+#################
+# API ENDPOINTS #
+#################
+
+@app.route('/sm')
+def sm():
+    return('API is working')
+
+
+@app.route('create/hostUser', methods=['POST'])
+def createHostUser():
+    pass
+
+
+@app.route('create/testUser', methods=['POST'])
+def createTestUser():
+    pass
+
+
+@app.route('check/userType', methods=['GET'])
+def checkUserType():
+    pass
 
 
 if __name__ == '__main__':
